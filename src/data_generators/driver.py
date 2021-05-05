d130e8a22ab6efe5961ad67dfe4d17ee29a6ac52
Ugur <uakyol@google.com>
2021-05-05 05:17:14 +0000

    initial commit

---
+++ src/data_generators/driver.py
@@ -0,0 +1,13 @@
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+
+def main(argv):
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+
+
+if __name__ == '__main__':
+  app.run(main)

27e341d8e49ec559dc9a6d4453da5f4b361cc389
Ugur <uakyol@google.com>
2021-05-10 04:14:06 +0000

    halfway there

--- src/data_generators/synthetic_data_generator.py
+++ src/data_generators/synthetic_data_generator.py
@@ -1,21 +1,3 @@
-"""TODO(uakyol): DO NOT SUBMIT without one-line documentation for synthetic_data_generator.
-
-TODO(uakyol): DO NOT SUBMIT without a detailed description of synthetic_data_generator.
-"""
-
-from absl import app
-from absl import flags
-
-FLAGS = flags.FLAGS
-
-
-def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
-
-
-if __name__ == '__main__':
-  app.run(main)
 # Copyright 2021 The Private Cardinality Estimation Framework Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -34,15 +16,27 @@ if __name__ == '__main__':
 from typing import List
 from wfa_planning_evaluation_framework.data_generators.data_design import DataDesign
 from wfa_planning_evaluation_framework.data_generators.data_set import DataSet
-from wfa_planning_evaluation_framework.data_generators.data_design_parameters import (
-    DataDesignParameters, DataSetParameters, OverlapModelParameters,
-    ImpressionGeneratorParameters, PricingGeneratorParameters)
+from wfa_planning_evaluation_framework.data_generators.data_set_parameters import (
+    DataSetParameters, GeneratorParameters)
 from wfa_planning_evaluation_framework.data_generators.pricing_generator import PricingGenerator
-from wfa_planning_evaluation_framework.data_generators.fixed_price_generator import  FixedPriceGenerator
+from wfa_planning_evaluation_framework.data_generators.fixed_price_generator import FixedPriceGenerator
 from wfa_planning_evaluation_framework.data_generators.impression_generator import ImpressionGenerator
 from wfa_planning_evaluation_framework.data_generators.homogeneous_impression_generator import HomogeneousImpressionGenerator
 from wfa_planning_evaluation_framework.data_generators.publisher_data import PublisherData
+from wfa_planning_evaluation_framework.data_generators.test_synthetic_data_design_config import TestSyntheticDataDesignConfig
+from wfa_planning_evaluation_framework.data_generators.synthetic_data_design_config import SyntheticDataDesignConfig
 from numpy.random import RandomState
+
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('output_folder', 'SomeFolder', 'Output Folder.')
+flags.DEFINE_string('data_design_config', 'TestConfig', 'Data Desgin Config.')
+
+name_to_config_dict = {'test': TestSyntheticDataDesignConfig}
+
 
 class SyntheticDataGenerator():
   """Generates a DataDesign with synthetic data derived from parameters.
@@ -52,52 +46,40 @@ class SyntheticDataGenerator():
     DataSet with different random seeds.
     """
 
-  def __init__(self, params: DataDesignParameters):
-    self._params = params
-    self.data_set_name = self.getName(params.data_set_parameters)
-
-  def getName(self, data_set_params: DataSetParameters):
-    #TODO(uakyol) : implement this after discussion.
-    return "homog_p=10_rep=3"
+  def __init__(self, output_folder: str, config: SyntheticDataDesignConfig):
+    self._config = config
+    self._output_folder = output_folder
 
   def __call__(self) -> DataDesign:
-    data_design = DataDesign(dirpath=self._params.output_folder)
-    for rep in range(self._params.num_reps):
-      data_design.add(
-          self.generateDataSet(self._params.data_set_parameters, rep))
+    data_design = DataDesign(dirpath=self._output_folder)
+    data_set_parameters_list = self._config()
+    for data_set_parameters in data_set_parameters_list:
+      data_design.add(self.generateDataSet(data_set_parameters))
     return data_design
 
-  def generateDataSet(self, params: DataSetParameters, seed: int) -> DataSet:
+  def generateDataSet(self, params: DataSetParameters) -> DataSet:
     publishers = []
+    publisher_size = params.largest_publisher_size
     for publisher in range(params.num_publishers):
       publishers.append(
           PublisherData.generate_publisher_data(
-              self.getImpressionGenerator(params.impression_generator_params,
-                                          seed),
-              self.getPricingGenerator(params.pricing_generator_params, seed),
+              params.impression_generator_params.generator(
+                  **params.impression_generator_params.params,
+                  n=publisher_size),
+              params.pricing_generator_params.generator(
+                  **params.pricing_generator_params.params),
               str(publisher)))
+      publisher_size = publisher_size * params.publisher_size_decay_rate
 
-    self.executeOverlap(publisher, params.overlap_model_params)
-    return DataSet(publishers, self.data_set_name +"_rs="+ str(seed))
+    return DataSet(publishers, params.name)
 
-  def executeOverlap(self, publisher: List[PublisherData],
-                     params: OverlapModelParameters):
-    #TODO(uakyol) : Use overlap model here when that class is implemented.
-    return
 
-  def getImpressionGenerator(self, params: ImpressionGeneratorParameters,
-                             seed: int) -> ImpressionGenerator:
-    if (params.impression_generator == "homogenous"):
-      return HomogeneousImpressionGenerator(params.num_users,
-                                            params.homogenous_lambda,
-                                            RandomState(seed))
-    else:
-      raise ValueError(
-          f"Invalid impression generator {params.impression_generator}")
+def main(argv):
+  data_generator = SyntheticDataGenerator(
+      FLAGS.output_folder, name_to_config_dict[FLAGS.data_design_config])
+  data_design = data_generator()
+  print(data_design)
 
-  def getPricingGenerator(self, params: PricingGeneratorParameters,
-                          seed: int) -> PricingGenerator:
-    if (params.pricing_generator == "fixed"):
-      return FixedPriceGenerator(params.fixed_price_cost)
-    else:
-      raise ValueError(f"Invalid price generator {params.pricin_generator}")
+
+if __name__ == '__main__':
+  app.run(main)
---
+++ src/data_generators/test_synthetic_data_design_config.py
@@ -0,0 +1,60 @@
+# Copyright 2021 The Private Cardinality Estimation Framework Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Encapculates the config for the test DataSet."""
+
+from typing import List
+
+from wfa_planning_evaluation_framework.data_generators.data_set_parameters import (
+    DataSetParameters, GeneratorParameters)
+from wfa_planning_evaluation_framework.data_generators.fixed_price_generator import FixedPriceGenerator
+from wfa_planning_evaluation_framework.data_generators.homogeneous_impression_generator import HomogeneousImpressionGenerator
+from wfa_planning_evaluation_framework.data_generators.sequentially_correlated_publisher_overlap_generator import SequentiallyCorrelatedPublisherOverlapGenerator
+from wfa_planning_evaluation_framework.data_generators.synthetic_data_design_config import SyntheticDataDesignConfig
+from numpy.random import RandomState
+
+
+class TestSyntheticDataDesignConfig(SyntheticDataDesignConfig):
+  """Generates a DataDesign with synthetic data derived from parameters.
+
+    This class
+    """
+
+  @staticmethod
+  def __call__(self) -> List[DataSetParameters]:
+    """Generates data set parameters to create a data set from.
+
+    Returns:
+       DataSetParameters object. This object can be hard coded or can be
+       constructed through some business logic.
+    """
+
+    return [getDataSetParams(i) for i in range(3)]
+
+  def getDataSetParams(seed: int):
+    return DataSetParameters(
+        num_publishers=3,
+        largest_publisher_size=100,
+        publisher_size_decay_rate=0.9,
+        pricing_generator_params=GeneratorParameters(
+            generator_type=FixedPriceGenerator, params={}),
+        impression_generator_params=GeneratorParameters(
+            generator_type=HomogeneousImpressionGenerator,
+            params={
+                "poisson_lambda": 0.1,
+                "random_state": RandomState(seed)
+            }),
+        overlap_generator_params=GeneratorParameters(
+            generator_type=SequentiallyCorrelatedPublisherOverlapGenerator,
+            params={}),
+        name="homog_p=10_pub=3_rs=" + str(seed))

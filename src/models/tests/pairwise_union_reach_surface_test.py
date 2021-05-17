7cead323822c76afef0f05a67a6e93ca716267a6
Ugur <uakyol@google.com>
2021-05-17 04:02:33 +0000

    cleaned up

--- src/models/tests/pairwise_union_reach_surface_test.py
+++ src/models/tests/pairwise_union_reach_surface_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for reach_surface.py."""
+"""Tests for pairwise_union_reach_surface.py."""
 
 from absl.testing import absltest
 import numpy as np

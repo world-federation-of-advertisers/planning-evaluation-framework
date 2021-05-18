b7ab06e061c1036c22e25384aa08b8f73184bd29
Ugur <uakyol@google.com>
2021-05-18 18:57:23 +0000

    cleaning up rebase

--- src/data_generators/homogeneous_impression_generator.py
+++ src/data_generators/homogeneous_impression_generator.py
@@ -1,3 +1,4 @@
+
 # Copyright 2021 The Private Cardinality Estimation Framework Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,65 +15,47 @@
 """Generate a stream of impressions from a poisson distr with fixed lambda."""
 
 from typing import List
-from numpy.random import Generator
+from numpy.random import RandomState
 
 from wfa_planning_evaluation_framework.data_generators.impression_generator import (
-    ImpressionGenerator,)
+    ImpressionGenerator,
+)
 
 
 class HomogeneousImpressionGenerator(ImpressionGenerator):
-<<<<<<< HEAD
-  """Generate a random sequence of viewer id's of ad impressions.
-
-    This class, along with PricingGenerator, assists in the generation of
-    random PublisherDataFiles.  The ImpressionGenerator will generate a
-    sequence of random impressions according to specified criteria.
-    """
-=======
     """Generate ad impressions using Poisson distribution with fixed lambda."""
->>>>>>> af9a0ab (Updates in response to comments from Jiayu.)
 
-  def __init__(self,
-               n: int,
-               poisson_lambda: float,
-               random_generator: Generator = None):
-    """Constructor for the HomogeneousImpressionGenerator.
-
+    def __init__(self, n: int, poisson_lambda: float, random_state: RandomState = None):
+        """Constructor for the HomogeneousImpressionGenerator.
         For each user, the number of impressions assigned to that user is
         determined by drawing from a shifted Poisson distribution with fixed
         parameter lambda.  The Poisson distribution is shifted by one.  E.g.,
         the PMF is given by Pr(X=k) = lambda^{k-1} e^{-lambda} / (k-1)!.
-
         Args:
           n:  The number of users.
           poisson_lambda:  The parameter of the Poisson distribution that
             determines viewing frequencies.
-          random_state:  An instance of numpy.random.RandomState that is used
-            for making draws from the Poisson distribution.
-    """
-    self._poisson_lambda = poisson_lambda
-    self._n = n
-    if random_generator:
-      self.random_generator = random_generator
-    else:
-      self.random_generator = Generator()
+          random_state:  An instance of numpy.random.RandomState that is
+            used for making draws from the Poisson distribution.
+        """
+        self._poisson_lambda = poisson_lambda
+        self._n = n
+        if random_state:
+            self._random_state = random_state
+        else:
+            self._random_state = RandomState()
 
-<<<<<<< HEAD
-  def __call__(self) -> List[int]:
-    """Generate a random sequence of impressions.
-=======
     def __call__(self) -> List[int]:
         """Generate impressions using Poisson distribution with fixed lambda.
->>>>>>> af9a0ab (Updates in response to comments from Jiayu.)
-
         Returns:
           A list of randomly generated user id's.  An id may occur multiple
           times in the output list, representing the fact that the user may
           see multiple ads from the publisher over the course of the campaign.
         """
-    impressions = []
-    for i in range(self._n):
-      impressions.extend([i] *
-                         (1 + self.random_generator.poisson(self._poisson_lambda)))
-    self.random_generator.shuffle(impressions)
-    return impressions
+        impressions = []
+        for i in range(self._n):
+            impressions.extend(
+                [i] * (1 + self._random_state.poisson(self._poisson_lambda))
+            )
+        self._random_state.shuffle(impressions)
+        return impressions

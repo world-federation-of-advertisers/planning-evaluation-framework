29f3a61bc1c36be51d826fb0a053593976c21720
uakyol <69486838+uakyol@users.noreply.github.com>
2021-05-27 14:14:17 -0700

    Revert "Dataset generator driver"

--- src/data_generators/independent_overlap_data_set.py
+++ src/data_generators/independent_overlap_data_set.py
@@ -13,9 +13,7 @@
 # limitations under the License.
 """Generate independent multi-pub data from single-pub data."""
 
-import math
 from typing import Iterable
-from numpy.random import Generator
 from numpy.random import RandomState
 from wfa_cardinality_estimation_evaluation_framework.simulations.set_generator import (
     IndependentSetGenerator,
@@ -38,31 +36,27 @@ class IndependentOverlapDataSet(OverlapDataSet):
         self,
         unlabeled_publisher_data_list: Iterable[PublisherData],
         universe_size: int,
-        random_generator: Generator,
+        random_state: RandomState = None,
         name: str = "independent",
     ) -> DataSet:
         """Constructor for IndependentOverlapDataSet.
 
         Args:
-          unlabeled_publisher_data_list:  a list of PublisherDataSet indicating
-            the reach curve of a publisher.
-          universe_size:  the universe size for applying the independent model
-            of overlap. Explicitly, for any two pubs 1 and 2, the overlap reach
-            between these two pubs 1 equals <pub 1 reach> * <pub 2 reach> /
-            universe_size.
-          random_generator: a random generator for generating the independent
-            reached ids.
-          name:  If specified, a human-readable name that will be associated to
-            this DataSet.
+          unlabeled_publisher_data_list:  a list of PublisherDataSet indicating the
+            reach curve of a publisher.
+          universe_size:  the universe size for applying the independent model of
+            overlap. Explicitly, for any two pubs 1 and 2, the overlap reach between
+            these two pubs 1 equals <pub 1 reach> * <pub 2 reach> / universe_size.
+          random_state: a random state for generating the independent reached ids.
+          name:  If specified, a human-readable name that will be associated to this
+            DataSet.
         """
         super().__init__(
             unlabeled_publisher_data_list=unlabeled_publisher_data_list,
             overlap_generator=IndependentSetGenerator,
             overlap_generator_kwargs={
                 "universe_size": universe_size,
-                "random_state": RandomState(
-                    seed=random_generator.integers(100000, size=1)[0]
-                ),
+                "random_state": random_state,
             },
             name=name,
         )

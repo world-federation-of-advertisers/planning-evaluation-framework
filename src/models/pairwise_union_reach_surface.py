1cc00c10a7c64c11b93e511ada716fc8f022cd30
Ugur <uakyol@google.com>
2021-06-02 21:41:55 +0000

    done

--- src/models/pairwise_union_reach_surface.py
+++ src/models/pairwise_union_reach_surface.py
@@ -25,149 +25,193 @@ from wfa_planning_evaluation_framework.models.reach_curve import ReachCurve
 
 
 class PairwiseUnionReachSurface(ReachSurface):
-  """Models reach with the pairwise union overlap model.
+    """Models reach with the pairwise union overlap model.
 
-     PairwiseUnionReachSurface models the combined reach with the formula
+    PairwiseUnionReachSurface models the combined reach with the formula
 
-     r_1(imp_1)+r_2(imp_2)+...+r_p(imp_p) -
-       1/2 sum_{j=1..p} sum_{k=1..p}
-       (a_{j,k} * {r_j(imp_j)r_k(imp_k)} / {max\{m_j,m_k}})
+    r_1(imp_1)+r_2(imp_2)+...+r_p(imp_p) -
+      1/2 sum_{j=1..p} sum_{k=1..p}
+      (a_{j,k} * {r_j(imp_j)r_k(imp_k)} / {max\{m_j,m_k}})
 
-     where:
-           p is the number of publishers
-           a_i_j are the model parameters we want to estimate
-           imp_i is the numnber of impressions for ith publisher
-           r_j(imp_j) is the reach of the jth publisher for imp_j impressions.
-
-
-  """
+    where:
+          p is the number of publishers
+          a_i_j are the model parameters we want to estimate
+          imp_i is the numnber of impressions for ith publisher
+          r_j(imp_j) is the reach of the jth publisher for imp_j impressions.
 
 
-  def __init__(self, reach_curves: Iterable[ReachCurve],
-               reach_points: Iterable[ReachPoint]):
-    """Constructor for PaiwiseUnionReachSurface.
-
-    Args:
-      reach_curves: A list of ReachCurves to be used in model fitting and reach
-        prediction.
-      reach_points: A list of ReachPoints to which the model is to be fit. This
-        is parallel to the reach_curves list. The reach point at ith poisition
-        is drawn from ith reach curve.
     """
 
-    self._reach_curves = copy.deepcopy(reach_curves)
-    self._n = len(reach_points)
-    self._p = len(reach_points[0].impressions)
-    super().__init__(data=reach_points, max_reach=0)
+    def __init__(
+        self, reach_curves: Iterable[ReachCurve], reach_points: Iterable[ReachPoint]
+    ):
+        """Constructor for PaiwiseUnionReachSurface.
 
-  def _fit(self) -> None:
-    z, alpha = self.get_z_and_alpha()
-    self._a = self.solve_a_given_z_and_alpha(z, alpha)
+        Args:
+          reach_curves: A list of ReachCurves to be used in model fitting and reach
+            prediction.
+          reach_points: A list of ReachPoints to which the model is to be fit. This
+            is parallel to the reach_curves list. The reach point at ith poisition
+            is drawn from ith reach curve.
+        """
 
-  def get_reach_vector(self, impressions: Iterable[int]) -> Iterable[int]:
-    """Calculates single publisher reaches for a given impression vector.
+        self._reach_curves = copy.deepcopy(reach_curves)
+        self._n = len(reach_points)
+        self._p = len(reach_points[0].impressions)
+        super().__init__(data=reach_points, max_reach=0)
 
-    Args:
-      impressions: A list of impressions per publisher.
-        prediction.
-    Returns:
-      Reach for each publisher.
-    """
-    return [
-        reach_curve.by_impressions(impression).reach()
-        for reach_curve, impression in zip(self._reach_curves, impressions)
-    ]
+    def _fit(self) -> None:
+        z, alpha = self.get_z_and_alpha()
+        self._a = self.solve_a_given_z_and_alpha(z, alpha)
 
-  def by_impressions(self,
-                     impressions: Iterable[int],
-                     max_frequency: int = 1) -> ReachPoint:
+    def get_reach_vector(self, impressions: Iterable[int]) -> Iterable[int]:
+        """Calculates single publisher reaches for a given impression vector.
 
-    reach_vector = self.get_reach_vector(impressions)
-    reach_sum = sum(reach_vector)
-    overlap = sum([
-        (self._a[i * self._p + j] * reach_vector[i] * reach_vector[j]) /
-        (max(self._reach_curves[i].max_reach, self._reach_curves[j].max_reach) *
-         2) for i in range(self._p) for j in range(self._p)
-    ])
+        Args:
+          impressions: A list of impressions per publisher. prediction.
 
-    return ReachPoint(impressions, [reach_sum - overlap])
+        Returns:
+          Reach for each publisher.
+        """
+        return [
+            reach_curve.by_impressions(impression).reach()
+            for reach_curve, impression in zip(self._reach_curves, impressions)
+        ]
 
-  def get_inequality_constraints(self):
-    """Returns the inequality constraint for the Cone Programming solver.
-       3 types of inequality constraints are stacked in matrix G and vector h:
+    def by_impressions(
+        self, impressions: Iterable[int], max_frequency: int = 1
+    ) -> ReachPoint:
 
-         G1: a_i_j >= 0 for all i,j -> all parameters are non negative
-         G2: sum_{j=1..p} a_i_j <= 1 for all i -> row sums are no more than one
-         G3: sum_{i=1..p} a_i_j <= 1 for all j -> col sums are no more than one
-    """
-    G1 = -matrix(np.eye(self._p * self._p))
-    G2 = np.kron(np.eye(self._p), np.ones(self._p))
-    G3 = np.kron(np.ones(self._p), np.eye(self._p))
-    h = matrix(
-        np.concatenate((np.zeros(self._p * self._p), np.ones(2 * self._p)),
-                       axis=None))
-    return matrix(np.vstack((G1, G2, G3))), h
+        reach_vector = self.get_reach_vector(impressions)
+        reach_sum = sum(reach_vector)
+        overlap = sum(
+            [
+                (self._a[i * self._p + j] * reach_vector[i] * reach_vector[j])
+                / (
+                    max(
+                        self._reach_curves[i].max_reach, self._reach_curves[j].max_reach
+                    )
+                    * 2
+                )
+                for i in range(self._p)
+                for j in range(self._p)
+            ]
+        )
 
-  def get_equality_constraints(self):
-    """Returns the equality constraint for the Cone Programming solver.
-       1 type of equality constraint is described by matrix A and vector b:
+        return ReachPoint(impressions, [reach_sum - overlap])
 
-         a_i_i = 0 for all i -> publishers don't have overlap with themselves.
-    """
-    A = np.zeros(shape=(self._p, self._p * self._p))
-    for i in range(self._p):
-      A[i, i * self._p + i] = 1
-    return matrix(A),  matrix(np.zeros(self._p))
+    def get_inequality_constraints(self):
+        """Returns the inequality constraint for the Quadratic Programming solver.
 
-  def solve_a_given_z_and_alpha(self, z: List[List[int]],
-                                alpha: List[List[int]]):
-    """Optimize for matrix a given z and alpha.
+        3 types of inequality constraints are stacked in matrix G and vector h:
 
-    Args:
-      z: a length-n list of length-(p * p) vectors.
-      alpha: a length-n list of scalars.
+          G1: a_i_j >= 0 for all i,j -> all parameters are non negative
+          G2: sum_{j=1..p} a_i_j <= 1 for all i -> row sums are no more than one
+          G3: sum_{i=1..p} a_i_j <= 1 for all j -> col sums are no more than one
+        """
+        G1 = -matrix(np.eye(self._p * self._p))
+        G2 = np.kron(np.eye(self._p), np.ones(self._p))
+        G3 = np.kron(np.ones(self._p), np.eye(self._p))
+        h = matrix(
+            np.concatenate(
+                (np.zeros(self._p * self._p), np.ones(2 * self._p)), axis=None
+            )
+        )
+        return matrix(np.vstack((G1, G2, G3))), h
 
-    Returns:
-      a length-(p * p) vector of the optimization problem above
-    """
+    def get_equality_constraints(self):
+        """Returns the equality constraint for the Quadratic Programming solver.
 
-    P = matrix(
-        sum([
-            np.matmul(z[k].reshape(self._p * self._p, 1),
-                      z[k].reshape(1, self._p * self._p))
+        1 type of equality constraint is described by matrix A and vector b:
+
+          a_i_i = 0 for all i -> publishers don't have overlap with themselves.
+        """
+        A = np.zeros(shape=(self._p, self._p * self._p))
+        for i in range(self._p):
+            A[i, i * self._p + i] = 1
+        return matrix(A), matrix(np.zeros(self._p))
+
+    def solve_a_given_z_and_alpha(self, z: List[List[int]], alpha: List[List[int]]):
+        """Optimize for matrix a given z and alpha using Quadratic Programming.
+
+        argmin_a \sum_{k=1..n} [y^{k} - alpha^{k} - a^T z^{k}]^2 =
+                  sum (a^T z^{k})^2 - 2 \sum [y^{k} - z^{k}] a^T z^{k} + ...
+
+        expanded,
+          P = sum_{k=1..n} z^{k} [z^{k}]^T,
+          and
+          q = - sum_{k=1..n} [y^{k} - alpha^{k}] z^{k},
+
+        and we minimize
+
+          argmin_a (1/2) x^T P a + q^T x$
+
+          s.t. Gx <= h
+               and
+               Ax=b,
+
+        Args:
+          z: a length-n list of length-(p * p) vectors.
+          alpha: a length-n list of scalars.
+
+        Returns:
+          a length-(p * p) vector of the optimization problem above
+        """
+
+        P = matrix(
+            sum(
+                [
+                    np.matmul(
+                        z[k].reshape(self._p * self._p, 1),
+                        z[k].reshape(1, self._p * self._p),
+                    )
+                    for k in range(self._n)
+                ]
+            )
+        )
+        q = -matrix(
+            sum((self._data[k].reach() - alpha[k]) * z[k] for k in range(self._n))
+        )
+
+        G, h = self.get_inequality_constraints()
+        A, b = self.get_equality_constraints()
+        res = solvers.qp(P, q, G, h, A, b)
+
+        return np.array(res["x"]).flatten()
+
+    def get_z_and_alpha(self):
+        """Get intermidiate quantities z and alpha.
+
+        Returns:
+          Tuple of z and alpha.
+          z: Length-n list of length-(p * p) vectors. Each vector is flattened
+          version of the matrix that denotes pairwise non-overlapped regions.
+          alpha: Length-n list of scalars. Each scalar denotes the overlapped reach
+          for that reach point.
+        """
+        reach_vectors = np.array(
+            [
+                self.get_reach_vector(reach_point.impressions)
+                for reach_point in self._data
+            ]
+        )
+
+        z = [
+            -np.array(
+                [
+                    (reach_vectors[k][i] * reach_vectors[k][j])
+                    / (
+                        max(
+                            self._reach_curves[i].max_reach,
+                            self._reach_curves[j].max_reach,
+                        )
+                        * 2
+                    )
+                    for i in range(self._p)
+                    for j in range(self._p)
+                ]
+            )
             for k in range(self._n)
-        ]))
-    q = -matrix(
-        sum((self._data[k].reach() - alpha[k]) * z[k] for k in range(self._n)))
-
-    G, h = self.get_inequality_constraints()
-    A, b = self.get_equality_constraints()
-    res = solvers.qp(P, q, G, h, A, b)
-
-    return np.array(res['x']).flatten()
-
-  def get_z_and_alpha(self):
-    """Get intermidiate quantities z and alpha.
-
-    Returns:
-      Tuple of z and alpha.
-      z: Length-n list of length-(p * p) vectors. Each vector is flattened
-      version of the matrix that denotes pairwise non-overlapped regions.
-      alpha: Length-n list of scalars. Each scalar denotes the overlapped reach
-      for that reach point.
-    """
-    reach_vectors = np.array([
-        self.get_reach_vector(reach_point.impressions)
-        for reach_point in self._data
-    ])
-
-    z = [
-        -np.array([(reach_vectors[k][i] * reach_vectors[k][j]) /
-                   (max(self._reach_curves[i].max_reach,
-                        self._reach_curves[j].max_reach) * 2)
-                   for i in range(self._p)
-                   for j in range(self._p)])
-        for k in range(self._n)
-    ]
-    alpha = [sum(reach_vector) for reach_vector in reach_vectors]
-    return z, alpha
+        ]
+        alpha = [sum(reach_vector) for reach_vector in reach_vectors]
+        return z, alpha

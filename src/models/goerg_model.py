76e7a54c177d9441cc5ec1a8d55fd3e456f8ab73
Jiayu Peng <jiayupeng@google.com>
2021-07-02 19:38:56 +0000

    trying to debug.

--- src/models/goerg_model.py
+++ src/models/goerg_model.py
@@ -41,14 +41,8 @@ class GoergModel(ReachCurve):
         """
         if len(data) != 1:
             raise ValueError("Exactly one ReachPoint must be specified")
-        if len(data[0].impressions) < 2:
-            warnings.warn(f"few impressions: {data[0].impressions}")
-        if data[0].impressions[0] < 5:
-            warnings.warn(f"small impression: {data[0].impressions[0]}")
-        if data[0].reach(1) < 5:
-            warnings.warn(f"small reach: {data[0].reach(1)}")
-        self._impressions = data[0].impressions[0]
-        self._reach = data[0].reach(1)
+        self._impressions = max(2, data[0].impressions[0])
+        self._reach = max(2, data[0].reach(1))
         self._fit()
         self._max_reach = self._rho
         if data[0].spends:

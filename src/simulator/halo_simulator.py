d20a2d233e5e862ed2682d451d6eb93b912ef901
Rieman Li <riemanli@google.com>
2021-07-22 10:40:18 -0700

    Add docstring.

--- src/simulator/halo_simulator.py
+++ src/simulator/halo_simulator.py
@@ -313,6 +313,22 @@ class HaloSimulator:
         sample_size: int,
         random_generator: np.random.Generator = np.random.default_rng(),
     ) -> Dict[int, List]:
+        """Return primitive regions with sampled reaches.
+
+        Args:
+            primitive_regions:  Contains k+ reaches in the regions. The k+
+              reaches for a given region is given as a list r[] where r[k] is
+              the number of people who were reached AT LEAST k+1 times.
+            sample_size:  The total number of sampled reach from the primitive
+              regions.
+            random_generator:  An instance of numpy.random.Generator that is
+              used for generating samples from a multivariate hypergeometric
+              distribution.
+        Returns:
+            A dictionary in which each key are the binary representations of
+              each primitive region of the Venn diagram, and each value is a
+              list of the reach in the corresponding region.
+        """
 
         region_repr_and_reach_pairs = [
             (region_repr, kplus_reaches[0])

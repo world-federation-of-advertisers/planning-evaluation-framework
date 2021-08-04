b7ca9a7f9a64dffb0acda317d7ba82f0f2306739
Matthew Clegg <{{USERNAME}}@google.com>
2021-08-04 14:34:02 -0700

    Initial work on multithreading.

--- src/driver/single_publisher_design.py
+++ src/driver/single_publisher_design.py
@@ -39,7 +39,8 @@ from wfa_planning_evaluation_framework.driver.trial_descriptor import (
     TrialDescriptor,
 )
 
-MODELING_STRATEGIES = ["goerg", "gamma_poisson"]
+# MODELING_STRATEGIES = ["goerg", "gamma_poisson"]
+MODELING_STRATEGIES = ["goerg"]
 CAMPAIGN_SPEND_FRACTIONS = list(np.arange(1,20) * 0.05)
 LIQUID_LEGIONS_DECAY_RATES = [10, 12, 15]
 LIQUID_LEGIONS_SKETCH_SIZES = [50_000, 100_000]

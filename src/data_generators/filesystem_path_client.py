cd67e2bb934871ccca28441f966c968e7222f962
Rieman Li <riemanli@google.com>
2021-10-08 17:40:33 -0700

    Add comments.

--- src/data_generators/filesystem_path_client.py
+++ src/data_generators/filesystem_path_client.py
@@ -5,6 +5,13 @@ from cloudpathlib import GSClient
 class FilesystemPathClient:
     def _get_path_module(self, path):
         if path.startswith("gs://"):
+            # When there is no credential provided, GSClients of cloudpathlib
+            # will create an anonymous client which can't access non-public
+            # buckets. On the other hand, Clients in google.cloud.storage will
+            # fall back to the default inferred from the environment. As a
+            # result, we first create a Client from google.cloud.storage and
+            # then use it to initiate a GSClient object and set it as the
+            # default for other operations.
             client = GSClient.get_default_client()
             return client.GSPath
 

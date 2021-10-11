33e40ba03270927541b366dcfda2a92b7e2d45c3
Rieman Li <riemanli@google.com>
2021-10-11 12:04:53 -0700

    Add docstring and clean up.

--- src/data_generators/filesystem_path_client.py
+++ src/data_generators/filesystem_path_client.py
@@ -1,20 +1,49 @@
+# Copyright 2021 The Private Cardinality Estimation Framework Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""A client class for managing filesystem-path like modules."""
+
 from pathlib import Path
 from cloudpathlib import GSClient
 from google.cloud import storage
 
 
+GSPATH_PREFIX = "gs://"
+
+
 class FilesystemPathClient:
+    """A client that manages different usages of filesystem-path like modules.
+
+    A FilesystemPathClient object takes as input a string of path and return
+    the corresponding filesystem-path object based on the prefix of the path.
+    """
+
     _default_gs_client = None
 
     @classmethod
     def get_default_gs_client(cls):
-        # When there is no credential provided, GSClients of cloudpathlib
-        # will create an anonymous client which can't access non-public
-        # buckets. On the other hand, Clients in google.cloud.storage will
-        # fall back to the default inferred from the environment. As a
-        # result, we first create a Client from google.cloud.storage and
-        # then use it to initiate a GSClient object and set it as the
-        # default for other operations.
+        """Get the default Google Cloud Storage client object used by cloudpathlib
+
+        When there is no credential provided, GSClient of cloudpathlib will
+        create an anonymous client which can't access non-public buckets. On
+        the other hand, Client in google.cloud.storage will fall back to the
+        default inferred from the environment. As a result, if there is no
+        default GSClient object, we first create a Client from
+        google.cloud.storage and then use it to initiate a GSClient object and
+        set it as the default for other operations. Then, the GSClient object
+        is returned.
+        """
+
         if cls._default_gs_client is None:
             cls._default_gs_client = GSClient(storage_client=storage.Client())
             cls._default_gs_client.set_as_default_client()
@@ -23,14 +52,17 @@ class FilesystemPathClient:
 
     @classmethod
     def reset_default_gs_client(cls):
+        """Reset the default GSClient"""
         cls._default_gs_client = None
 
-    def _get_path_module(self, path):
-        if path.startswith("gs://"):
+    def _get_path_module(self, path: str):
+        """Get the filesystem path module according to the prefix of input path"""
+        if path.startswith(GSPATH_PREFIX):
             return self.get_default_gs_client().GSPath
 
         return Path
 
-    def get_fs_path(self, path):
+    def get_fs_path(self, path: str):
+        """Return a filesystem path object"""
         fs_path_module = self._get_path_module(path)
         return fs_path_module(path)
--- src/data_generators/tests/filesystem_path_client_test.py
+++ src/data_generators/tests/filesystem_path_client_test.py
@@ -1,11 +1,23 @@
+# Copyright 2021 The Private Cardinality Estimation Framework Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for filesystem_path_client.py."""
+
 from absl.testing import absltest
-from tempfile import TemporaryDirectory
 from unittest.mock import patch
 
 from pathlib import Path
-from cloudpathlib import GSClient, GSPath
-from google.cloud import storage
-from cloudpathlib.local import LocalGSClient, LocalGSPath
+from cloudpathlib import GSPath
 import wfa_planning_evaluation_framework.data_generators.filesystem_path_client as filesystem_path_client
 from wfa_planning_evaluation_framework.data_generators.filesystem_path_client import (
     FilesystemPathClient,

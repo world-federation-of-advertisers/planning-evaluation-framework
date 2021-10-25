ea675f4f6055b4b6af6024f7ed529a445ddb84e3
Rieman Li <riemanli@google.com>
2021-10-25 18:39:42 +0800

    Modify experiment_driver.py and its unit tests to use filesystem wrappers.

--- src/driver/tests/experiment_driver_test.py
+++ src/driver/tests/experiment_driver_test.py
@@ -18,11 +18,12 @@ import numpy as np
 import pandas as pd
 from tempfile import TemporaryDirectory
 from unittest.mock import patch
-from pathlib import Path
-from cloudpathlib.local import LocalGSClient, LocalGSPath
-import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
+import pathlib
 import logging
+
+import cloudpathlib.local
+import apache_beam as beam
+import apache_beam.options.pipeline_options as pipeline_options
 
 import wfa_planning_evaluation_framework.driver.experiment_driver as experiment_driver
 import wfa_planning_evaluation_framework.driver.experimental_design as experimental_design
@@ -43,10 +44,17 @@ from wfa_planning_evaluation_framework.driver import (
     analysis_example_experimental_design,
 )
 from wfa_planning_evaluation_framework.driver import single_publisher_design
+from wfa_planning_evaluation_framework.filesystem_wrapper import (
+    filesystem_pathlib_wrapper,
+)
+from wfa_planning_evaluation_framework.filesystem_wrapper import (
+    filesystem_cloudpath_wrapper,
+)
+import wfa_planning_evaluation_framework.driver.tests.experimental_design_test as experimental_design_test
 
 
 class FakeExperimentalTrial(ExperimentalTrial):
-    def evaluate(self, seed: int):
+    def evaluate(self, seed: int, filesystem=None):
         return pd.DataFrame(
             {
                 "data_set_name": [self._data_set_name],
@@ -55,20 +63,14 @@ class FakeExperimentalTrial(ExperimentalTrial):
         )
 
 
-class FakeEvaluateTrialDoFn(beam.DoFn):
-    def process(self, trial, seed):
-        import pandas as pd
-
-        yield pd.DataFrame({"col": [1]})
-
-
 class ExperimentDriverTest(absltest.TestCase):
     def setUp(self):
-        self.client = LocalGSClient()
+        self.client = cloudpathlib.local.LocalGSClient()
         self.client.set_as_default_client()
 
     def tearDown(self):
-        LocalGSClient.reset_default_storage_dir()
+        cloudpathlib.local.LocalGSClient.reset_default_storage_dir()
+        cloudpathlib.local.localclient.clean_temp_dirs()
 
     def test_sample_experimental_design(self):
         sample_design = sample_experimental_design.generate_experimental_design_config(
@@ -151,27 +153,47 @@ class ExperimentDriverTest(absltest.TestCase):
             result = experiment_driver.execute()
             self.assertEqual(result.shape[0], 2592)
 
-    @patch.object(experimental_design, "EvaluateTrialDoFn", FakeEvaluateTrialDoFn)
+    @patch.object(
+        experimental_design,
+        "EvaluateTrialDoFn",
+        experimental_design_test.FakeEvaluateTrialDoFn,
+    )
     def test_experiment_driver_using_apache_beam_locally(self):
         with TemporaryDirectory() as d:
             data_design_dir = d + "/data"
             output_file = d + "/output.csv"
             intermediate_dir = d + "/intermediates"
-            Path(data_design_dir).mkdir(parents=True, exist_ok=True)
-            Path(intermediate_dir).mkdir(parents=True, exist_ok=True)
+            pathlib.Path(data_design_dir).mkdir(parents=True, exist_ok=True)
+            pathlib.Path(intermediate_dir).mkdir(parents=True, exist_ok=True)
 
+            filesystem = filesystem_pathlib_wrapper.FilesystemPathlibWrapper()
             result = self._execute_experiment_driver_with_sample_experimental_design_using_apache_beam(
-                data_design_dir, output_file, intermediate_dir
+                data_design_dir, output_file, intermediate_dir, filesystem
             )
             self.assertEqual(result.shape[0], 2700)
 
-    @patch.object(experiment_driver, "GSClient", LocalGSClient)
-    @patch.object(experimental_design, "GSClient", LocalGSClient)
-    @patch.object(experimental_design, "EvaluateTrialDoFn", FakeEvaluateTrialDoFn)
-    @patch.object(data_set, "GSPath", LocalGSPath)
-    @patch.object(data_design, "GSPath", LocalGSPath)
+    @patch.object(
+        experimental_design,
+        "EvaluateTrialDoFn",
+        experimental_design_test.FakeEvaluateTrialDoFn,
+    )
+    @patch.object(
+        filesystem_cloudpath_wrapper,
+        "CloudPath",
+        cloudpathlib.local.LocalGSPath,
+    )
+    @patch.object(
+        filesystem_cloudpath_wrapper.FilesystemCloudpathWrapper,
+        "set_default_client_to_gs_client",
+        cloudpathlib.local.LocalGSClient.get_default_client,
+    )
+    @patch.object(
+        filesystem_cloudpath_wrapper.FilesystemCloudpathWrapper,
+        "open",
+        experimental_design_test.fake_open,
+    )
     def test_experiment_driver_using_apache_beam_and_cloud_path(self):
-        parent_dir_path = self.client.GSPath("gs://ExperimentDriverTest/parent")
+        parent_dir_path = self.client.CloudPath("gs://ExperimentDriverTest/parent")
         data_design_dir_path = parent_dir_path.joinpath("data_design")
         output_file_path = parent_dir_path.joinpath("output.csv")
         intermediate_dir_path = parent_dir_path.joinpath("intermediates")
@@ -181,17 +203,36 @@ class ExperimentDriverTest(absltest.TestCase):
         intermediate_dir_path.joinpath("dummy.txt").write_text(
             "For creating the target directory."
         )
+        filesystem = filesystem_cloudpath_wrapper.FilesystemCloudpathWrapper()
 
         result = self._execute_experiment_driver_with_sample_experimental_design_using_apache_beam(
-            str(data_design_dir_path), str(output_file_path), str(intermediate_dir_path)
+            str(data_design_dir_path),
+            str(output_file_path),
+            str(intermediate_dir_path),
+            filesystem,
+            use_cloud_path=True,
         )
         self.assertEqual(result.shape[0], 2700)
 
     def _execute_experiment_driver_with_sample_experimental_design_using_apache_beam(
-        self, data_design_dir, output_file, intermediate_dir
+        self,
+        data_design_dir,
+        output_file,
+        intermediate_dir,
+        filesystem,
+        use_cloud_path=False,
     ):
+        data_design_dir_for_generator = None
+        if use_cloud_path:
+            data_design_dir_cloud_path = self.client.CloudPath(data_design_dir)
+            data_design_dir_for_generator = self.client._cloud_path_to_local(
+                data_design_dir_cloud_path
+            )
+        else:
+            data_design_dir_for_generator = data_design_dir
+
         data_design_generator = SyntheticDataDesignGenerator(
-            data_design_dir, simple_data_design_example.__file__, 1, False
+            data_design_dir_for_generator, simple_data_design_example.__file__, 1, False
         )
         data_design_generator()
         experimental_design = sample_experimental_design.__file__
@@ -206,20 +247,31 @@ class ExperimentDriverTest(absltest.TestCase):
             cores=num_workers,
         )
 
+        temp_location = None
+        if use_cloud_path:
+            intermediate_dir_cloud_path = self.client.CloudPath(intermediate_dir)
+            temp_location = self.client._cloud_path_to_local(
+                intermediate_dir_cloud_path
+            )
+        else:
+            temp_location = intermediate_dir
+
         pipeline_args = []
         pipeline_args.extend(
             [
                 "--runner=direct",
                 "--direct_running_mode=multi_processing",
-                f"--temp_location={intermediate_dir}",
+                f"--temp_location={temp_location}",
                 f"--direct_num_workers={num_workers}",
             ]
         )
-        pipeline_options = PipelineOptions(pipeline_args)
+        options = pipeline_options.PipelineOptions(pipeline_args)
 
         logging.disable(logging.CRITICAL)
         result = experiment_driver.execute(
-            use_apache_beam=True, pipeline_options=pipeline_options, client=self.client
+            use_apache_beam=True,
+            pipeline_options=options,
+            filesystem=filesystem,
         )
         logging.disable(logging.NOTSET)
         return result
